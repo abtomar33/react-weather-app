8ed9dca79e10e1c89349d22a285de48a31d45091
ABHISHEK TOMER <abhishek@TSPI.TECH>
2020-08-14 23:47:18 +0530

    Updates

---
+++ precache-manifest.69f5b795b18b2d8c866777bb2ab5c564.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e11bf3bf637b9bfca6160f96605166",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "2267cdf64830d9dc9503",
+    "url": "/react-weather-app/static/css/main.78606db5.chunk.css"
+  },
+  {
+    "revision": "04e46966fd38d120754a",
+    "url": "/react-weather-app/static/js/2.de209398.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.de209398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2267cdf64830d9dc9503",
+    "url": "/react-weather-app/static/js/main.127c0fc3.chunk.js"
+  },
+  {
+    "revision": "aacb36d1c4af6b3eab1b",
+    "url": "/react-weather-app/static/js/runtime-main.d770b3fe.js"
+  }
+]);
